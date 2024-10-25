c70f9a808c592842c2fe68415ce2ae1c5186e153
Arviey <rohit42196@gmail.com>
2024-10-25 12:14:46 +0530

    feat:
    Server configured

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
+let dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+
+const PORT = dotenv.parsed.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
