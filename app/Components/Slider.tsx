5ed143d65df0ff24b2d9e86b9ae8ab251428c998
arvi-ey <tuhinroyand@gmail.com>
2024-11-19 00:14:04 +0530

    fix
    Auto sliding issue fixed

--- app/Components/Slider.tsx
+++ app/Components/Slider.tsx
@@ -3,6 +3,7 @@ import { View, FlatList, Image, Dimensions, FlatListProps, Text } from 'react-na
 import { Colors } from '@/Theme';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Banner } from '@/Redux/Slice/bannerSlicer';
+import index from '../../web/src/components/Analytics/index';
 interface SliderProps {
     data: Banner[];
 }
@@ -12,25 +13,30 @@ const { width } = Dimensions.get('window');
 const Slider: React.FC<SliderProps> = ({ data }) => {
     const flatListRef = useRef<FlatList<Banner>>(null);
     const theme = useColorScheme();
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setCurrentIndex((prevIndex) => {
-    //             // console.log(data.length)
-    //             const nextIndex = prevIndex === data.length - 1 ? 0 : prevIndex + 1;
-    //             flatListRef.current?.scrollToIndex({ index: nextIndex, animated: true });
-    //             return nextIndex;
-    //         });
-    //     }, 2500);
-    //     return () => clearInterval(interval);
-    // }, []);
-
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => {
+                const nextIndex = prevIndex === data.length - 1 ? 0 : prevIndex + 1;
+                flatListRef.current?.scrollToIndex({ index: nextIndex, animated: true });
+                return nextIndex;
+            });
+        }, 2000);
+        return () => clearInterval(interval);
+    }, [data]);
     const renderItem: FlatListProps<Banner>['renderItem'] = ({ item }) => (
         <View style={{ width, alignItems: 'center', }}>
             <Image source={{ uri: item.image }} style={{ width: width - 20, borderRadius: 12, height: 450 }} resizeMode="stretch" />
         </View>
     );
+
+    const HandleSCroll = (event: any) => {
+        const offset = event.nativeEvent.contentOffset.x
+        const index = parseInt((offset / width).toFixed())
+        setCurrentIndex(index)
+
+    }
 
     return (
         <View style={{ marginVertical: 10 }} >
@@ -40,6 +46,7 @@ const Slider: React.FC<SliderProps> = ({ data }) => {
                 renderItem={renderItem}
                 keyExtractor={(item, index) => index.toString()}
                 horizontal
+                onScroll={HandleSCroll}
                 pagingEnabled
                 showsHorizontalScrollIndicator={false}
             />
